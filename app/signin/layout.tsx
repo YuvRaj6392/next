5018e89183a7628b9a628a4d40000c998b0b7d56
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2024-04-30 01:01:01 +0530

    added

---
+++ app/signin/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function layout({children}:{children:React.ReactNode}) {
+  return (
+   <>
+   <div className="text-center">
+
+20% off on our products 
+
+</div>
+{children}
+   </>
+    
+    
+  )
+}
